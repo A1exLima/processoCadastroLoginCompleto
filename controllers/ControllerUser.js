9b0d92ceac00c897566fd03c6d2f3564a0f7829e
Alima07 <alex.lima_dev@outlook.com>
2022-10-01 07:25:32 -0300

    Aplicacao da biblioteca express-validator para validacao dos formulários

--- controllers/ControllerUser.js
+++ controllers/ControllerUser.js
@@ -1,12 +1,23 @@
+const { validationResult } = require('express-validator');
+
 module.exports = {
 
     registration: (req, res) =>{
 
-        res.render('userRegister.ejs');
+        res.render('userRegister.ejs', {uploadImg: true});
     },
 
     processRegistration: (req, res) =>{
 
-        res.send('Imagem enviada com sucesso');
+        let erros = validationResult(req);
+            
+        if(!erros.isEmpty() || req.file == undefined){
+
+            return res.render('userRegister.ejs', { erros: erros.mapped(), old: req.body, uploadImg: false});
+
+        } else{
+
+            res.send("CADASTRO EFETUADO COM SUCESSO");
+        }    
     } 
 }
--- routers/RouterUser.js
+++ routers/RouterUser.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+
 const multerDiskStorage = require('..//middlewares/multerDiskStorage');
 const upload = multer({storage: multerDiskStorage});
 
+//------------------------------------------------------------//
+
+const { body } = require('express-validator');
+
+const validations = [
+    body("name")
+        .notEmpty().withMessage("O Nome deve ser preenchido").bail()
+        .isLength({min:3}).withMessage("O Nome deve conter no mínimo 3 caracteres"),
+    body("email")
+        .notEmpty().withMessage("O E-mail deve ser preenchido").bail()
+        .isEmail().withMessage("Deve preencher com um E-mail válido"),
+    body("password")
+        .notEmpty().withMessage("A Senha deve ser preenchida").bail()
+        .isLength({min:5}).withMessage("A Senha deve conter no mínimo 5 caracteres")
+];
+    
 //---------------------------------------------------------
 
 const ControllerUser = require('../controllers/ControllerUser');
 router.get('/registration', ControllerUser.registration);
 
-router.post('/registration', upload.single('imageUser'), ControllerUser.processRegistration);
+router.post('/registration', upload.single('imageUser') , validations , ControllerUser.processRegistration);
 
 //---------------------------------------------------------
 
